90dcad32f89ab7777a65c05cd1a3b2cafb3eda85
Ikenna Richard <oguejioforrichard@gmail.com>
2024-02-12 10:49:17 +0100

    Update style path and page theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import Pulse from './components/pulse/Pulse';
-import SplashScreen from './components/Splash/SplashScreen';
+import SplashScreen from './components/splash/SplashScreen';
 import NavBar from './components/navBar/NavBar';
 import Hero from './components/hero/Hero';
 import Project from './components/projects/Projects';
@@ -8,13 +8,13 @@ import ProjectList from './components/projects/ProjectList';
 import Contact from './components/contact/Contact';
 import About from './components/about/About';
 import Footer from './components/footer/Footer';
-import './app.sass'
+import { ThemeContext } from './context/ThemeContext';
+import './styles/app.sass'
 
 
 function App() {
-
   const [isLoading, setIsLoading] = useState(true);
-    
+  const { isDarkMode } = useContext(ThemeContext);
       useEffect(() => {
         setTimeout(() => {
           setIsLoading(false);
@@ -25,26 +25,26 @@ function App() {
         return <SplashScreen />;
       }
   return (
-    <>
+    <div className={isDarkMode ? 'dark' : null}>
       <Pulse/>
-      <section id='home'>
+      <section id='home' className='background-dark text-dark'>
         <NavBar/>
         <Hero/>
       </section>
-      <section id='about'>
+      <section id='about' className='background-dark text-dark'>
         <About/>
       </section>
-      <section id='projects'>
+      <section id='projects' className='background-dark text-dark'>
         <Project/>
       </section>
       
         <ProjectList/>
 
-      <section id='contact'>
+      <section id='contact' className='background-dark text-dark'>
         <Contact/>
       </section>
       <Footer/>
-    </>
+    </div>
   )
 }
 
