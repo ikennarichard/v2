474de0fa2cbbad09f30384c59d417cf59bd95d06
Ikenna Richard <oguejioforrichard@gmail.com>
2024-02-12 10:41:23 +0100

    Add context for page theme

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState, useEffect } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(null);
+
+  // Retrieve initial theme from local storage or default to false (light mode)
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('darkMode');
+      setIsDarkMode(storedTheme ? JSON.parse(storedTheme) : false);
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode !==  null) {
+      localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
+    }
+  }, [isDarkMode]);
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
