7dc114ea1e4305cb8b14e1fb40b3d4cbb815f62e
Ikenna Richard <oguejioforrichard@gmail.com>
2024-03-09 21:25:47 +0100

    refactor: ♻️ Update tech stack and resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -30,13 +30,20 @@ const About = () => {
         >
           <ul>
             <li><p>HTML/CSS</p></li>
-            <li><p>JavaScript</p></li>
+            <li><p>JavaScript / TypeScript</p></li>
             <li><p>React</p></li>
+            <li><p>Astro</p></li>
             <li><p>Redux</p></li>
+            <li><p>Node.js</p></li>
+            <li><p>Go</p></li>
             <li><p>Ruby on Rails</p></li>
+            <li><p>Express.js</p></li>
             <li><p>Bootstrap</p></li>
-            <li><p>Git</p></li>
-            <li><p>Jest / Vitest</p></li>
+            <li><p>Tailwind CSS</p></li>
+            <li><p>SASS</p></li>
+            <li><p>Git / Github</p></li>
+            <li><p>Jest / Vitest / React Testing Library</p></li>
+            <li><p>Pair programming</p></li>
             <li><p>Agile kanban development process</p></li>
             <li><p>RESTFul API</p></li>
           </ul>
@@ -49,9 +56,9 @@ const About = () => {
         <div className="details">
           <h2>About</h2>
           <div className="text">
-            <h3>I&apos;m Richard. A freelance software engineer. A writer and problem solver.</h3>
-            <p>My journey into tech took an exciting turn when I joined the Microverse web development programme. Since then, I fell in love with React and Redux and have been using them ever since in web applications.</p>
-            <p>In addition to React, I have experience working with Ruby on Rails and PostgreSQL.</p>
+            <h3>I&apos;m Richard. A freelance software engineer</h3>
+            <p>My journey into tech took an exciting turn when I joined the Microverse software development bootcamp. Since then, I fell in love with React and Redux and have been using them ever since in web applications.</p>
+            <p>In addition to React, I have experience working with backend technologies such as Ruby on Rails, Express.js and PostgreSQL.</p>
             <p>What excites me most about my job is being able to create things that provide high value to people.</p>
           </div>
         </div>
