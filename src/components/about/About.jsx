a60805c5868e9e49d8ace20dcb14e1e13f2fbdac
Ikenna Richard <oguejioforrichard@gmail.com>
2025-01-31 00:17:45 +0100

    feat:update text animation

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
-import { motion } from 'framer-motion'
-import './about.sass'
-import { useRef } from 'react'
-import { useScroll, useTransform } from 'framer-motion'
+import { motion } from "framer-motion";
+import "./about.sass";
+import { useRef } from "react";
+import { useScroll, useTransform } from "framer-motion";
 
 const About = () => {
   const ref = useRef();
@@ -11,46 +11,90 @@ const About = () => {
     offset: ["start end", "center", "end start"],
   });
 
-  const scaleProgress = useTransform(scrollYProgress, [0, 0.5, 0.6, 1], [0.8, 1, 1, 0.6]);
-  const opacityProgress = useTransform(scrollYProgress, [0, 0.5, 1], [0.8, 1, 0.5]);
+  const scaleProgress = useTransform(
+    scrollYProgress,
+    [0, 0.5, 0.6, 1],
+    [0.8, 1, 1, 0.6]
+  );
+  const opacityProgress = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    [0.8, 1, 0.5]
+  );
+
   return (
     <div className="about">
       <div className="wrapper">
-        <div 
-          className="info"
-          
-        >
+        <div className="info">
           <ul>
-            <li><p>HTML/CSS</p></li>
-            <li><p>JavaScript / TypeScript</p></li>
-            <li><p>React</p></li>
-            <li><p>Astro</p></li>
-            <li><p>Redux</p></li>
-            <li><p>Bootstrap</p></li>
-            <li><p>Tailwind CSS</p></li>
-            <li><p>SASS</p></li>
-            <li><p>Git / Github</p></li>
-            <li><p>Jest / Vitest / React Testing Library</p></li>
+            <li>
+              <p>HTML/CSS</p>
+            </li>
+            <li>
+              <p>JavaScript / TypeScript</p>
+            </li>
+            <li>
+              <p>Reactjs/ React Native</p>
+            </li>
+            <li>
+              <p>Nextjs</p>
+            </li>
+            <li>
+              <p>Go</p>
+            </li>
+            <li>
+              <p>Redux</p>
+            </li>
+            <li>
+              <p>Tailwind CSS</p>
+            </li>
+            <li>
+              <p>SASS</p>
+            </li>
+            <li>
+              <p>Git / Github</p>
+            </li>
+            <li>
+              <p>Jest / Vitest / React Testing Library</p>
+            </li>
           </ul>
+          <span
+            style={{
+              fontSize: "6rem",
+            }}
+          >
+            ...
+          </span>
         </div>
-        <motion.div 
+        <motion.div
           className="details"
           ref={ref}
           style={{
             scale: scaleProgress,
-            opacity: opacityProgress
+            opacity: opacityProgress,
           }}
         >
           <h2>ABOUT</h2>
           <div className="text">
-          <p>My journey in technology has been a thrilling adventure, particularly since joining the Microverse software development bootcamp. It was there that I discovered my passion for creating user interfaces, leading me to specialize in frontend development with React, Astro and Redux.</p>
+            <p>
+              Hi, i am water, a software engineer. HNG-trained and obsessed with
+              high-performance UIs with precision & style, I make sure every UI
+              feels just right. I tweak, tinker, and optimize—because great
+              experiences shouldn’t just work, they should wow! 🚀
+            </p>
 
-          <p>In addition to my frontend focus, I&apos;ve also gained valuable experience with backend technologies like Ruby on Rails, Express.js, and PostgreSQL. My full-stack background has given me the skills to seamlessly blend frontend and backend components.</p>
+            <p>
+              I&apos;ve also gained valuable experience with backend
+              technologies like Ruby on Rails, Go, and PostgreSQL. My approach
+              is holistic; I believe in collaboration 🎭, from UI/UX designers
+              to backend developers, to create a cohesive and impactful
+              product.⚡
+            </p>
           </div>
         </motion.div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,59 @@
 import "./hero.sass";
 import { useContext, useRef } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
-import { motion, useTransform } from "framer-motion";
+import { delay, motion, useTransform } from "framer-motion";
 import { useScroll } from "framer-motion";
 import TimeDisplay from "../time-display/time-display";
 
+const letterVariants = {
+  hidden: { x: -100, opacity: 0, rotate: -10 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+    transition: {
+      ease: [0.175, 0.885, 0.32, 1.275],
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const jobTitleVariants = {
+  hidden: { y: 20, opacity: 0, scale: 1.9 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.9,
+      ease: [0.175, 0.885, 0.32, 1.275],
+      duration: 1,
+    },
+  },
+};
+
+const renderLetters = (name) => {
+  if (!name) return null;
+  return (
+    <motion.span
+      initial="hidden"
+      animate="visible"
+      variants={letterVariants}
+      className="inline-block"
+    >
+      {name.split("").map((letter, index) => (
+        <motion.span
+          key={index}
+          className="inline-block"
+          variants={letterVariants}
+        >
+          {letter}
+        </motion.span>
+      ))}
+    </motion.span>
+  );
+};
 
 const sliderVariants = {
   initial: {
@@ -35,7 +84,7 @@ const Hero = () => {
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [1, 0]);
 
   const { isDarkMode } = useContext(ThemeContext);
-  
+
   return (
     <div className="hero">
       <div className="wrapper">
@@ -45,16 +94,19 @@ const Hero = () => {
           style={{
             scale: scaleProgress,
             opacity: opacityProgress,
-            marginBlockStart: '1rem',
+            marginBlockStart: "1rem",
           }}
         >
-          <TimeDisplay />
+          {/* <TimeDisplay /> */}
           <p>Hello, my name is</p>
-          <h2>IKENNA RICHARD</h2>
-          <p
+          <h2> {renderLetters("IKENNA RICHARD")}</h2>
+          <motion.p
+            initial="hidden"
+            animate="visible"
+            variants={jobTitleVariants}
           >
-            Frontend Engineer
-          </p>
+            Software Engineer
+          </motion.p>
           <motion.div
             className={`buttons 
             ${isDarkMode ? "border-dark" : null}`}
