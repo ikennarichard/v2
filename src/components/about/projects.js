685d3e168a28a98ada78ce4a098ab3a12f71f2db
Ikenna Richard <oguejioforrichard@gmail.com>
2025-11-01 02:54:17 +0100

    refactor: update about ui add ssource and url icons for project list

---
+++ src/components/about/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+    {
+    "id": 1,
+    "title": "Expenses Chart",
+    "source": "https://github.com/ikennarichard/Expenses-chart-component",
+    "url": "https://ikennarichard.github.io/Expenses-chart-component"
+    },
+
+    {
+      "id": 2,
+      "title": "Space Tourism",
+      "source": "https://github.com/ikennarichard/Space-tourism",
+      "url": "https://rich-space.netlify.app/"
+    },
+
+    {
+      "id": 3,
+      "title": "Chess Game",
+      "source": "https://github.com/ikennarichard/chess-game",
+      "url": "https://sparkling-praline-3668b1.netlify.app"
+    }
+  ]
